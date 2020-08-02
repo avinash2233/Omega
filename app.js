25bb068be2195de973622a96f752667d97b838fc
avinash2233 <67163382+avinash2233@users.noreply.github.com>
2020-08-02 20:17:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBsqRAx-o-6zr5vMF12Pa9_WC_EaE3SFaM",
+  authDomain: "hacking-931dc.firebaseapp.com",
+  databaseURL: "https://hacking-931dc.firebaseio.com",
+  projectId: "hacking-931dc",
+  storageBucket: "hacking-931dc.appspot.com",
+  messagingSenderId: "706855577740",
+  appId: "1:706855577740:web:48b64834c60560208af3a9",
+  measurementId: "G-VP2QVV36WP"
 };
-
      
  
 ////Do Not Edit Below This
